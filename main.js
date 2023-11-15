b1b5b1a9e1818b7bd597250ff03a9b07687a6701
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-15 10:19:34 +0100

    function

--- main.js
+++ main.js
@@ -55,79 +55,72 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
-/*<div class="post">
-            <div class="post__header">
-                <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
-                    </div>
-                    <div class="post-meta__data">
-                        <div class="post-meta__author">Phil Mangione</div>
-                        <div class="post-meta__time">4 mesi fa</div>
-                    </div>                    
-                </div>
-            </div>
-            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-            <div class="post__image">
-                <img src="https://unsplash.it/600/300?image=171" alt="">
-            </div>
-            <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>
-                    </div>
-                    <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-                    </div>
-                </div> 
-            </div>            
-        </div> */
+
 let contain = document.getElementById("container")
-for (let i = 0; i < posts.length; i++) {
-    //const element = array[i];
-    const firstCicle = posts.forEach((element) => { 
-        contain.innerHTML += `<div class=post>
-                            <div class = "post_header">
-                                <div class= "post-meta">
-                                    <div class="post-meta__icon">
-                                        <img class="profile-pic" src="${element.author.image}" alt="Phil Mangione">
+
+contain += postTemplate()
+function postTemplate() {
+    for (let i = 0; i < posts.length; i++) {
+        //const element = array[i];
+        const firstCicle = posts.forEach((element) => { 
+            
+            contain.innerHTML += `<div class=post>
+                                <div class = "post_header">
+                                    <div class= "post-meta">
+                                        <div class="post-meta__icon">
+                                            <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">
+                                        </div>
+                                        <div class="post-meta__data">
+                                            <div class="post-meta__author">${element.author.name}</div>
+                                            <div class="post-meta__time">${element.created}</div>
+                                        </div> 
                                     </div>
-                                    <div class="post-meta__data">
-                                        <div class="post-meta__author">${element.author.name}</div>
-                                        <div class="post-meta__time">${element.created}</div>
-                                    </div> 
                                 </div>
-                            </div>
-                            <div class="post__text">${element.content}</div>
-                            <div class="post__image">
-                                <img src="${element.media}" alt="">
-                            </div>
-                            <div class="post__footer">
-                                <div class="likes js-likes">
-                                    <div class="likes__cta">
-                                        <a class="like-button  js-like-button" href="#" data-postid="1">
-                                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                                            <span class="like-button__label">Mi Piace</span>
-                                        </a>
-                                    </div>
-                                    <div class="likes__counter">
-                                           Piace a <b id="like-counter-1" class="js-likes-counter"></b> persone
-                                    </div>
-                                </div> 
-                            </div>               
-                            </div>` 
-    })
+                                <div class="post__text">${element.content}</div>
+                                <div class="post__image">
+                                    <img src="${element.media}" alt="">
+                                </div>
+                                <div class="post__footer">
+                                    <div class="likes js-likes">
+                                        <div class="likes__cta">
+                                            <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
+                                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                                <span class="like-button__label">Mi Piace</span>
+                                            </a>
+                                        </div>
+                                        <div class="likes__counter">
+                                               Piace a <b id="like-counter-${element.id}
+                                               " class="js-likes-counter">${element.likes}</b> persone
+                                        </div>
+                                    </div> 
+                                </div>               
+                                </div>` 
+        })
+        return firstCicle;
+    }
 }
-let startLike = 0
-for (let i = 0; i < 100; i++) {
-    const indexLike =i;
-    console.log(indexLike)
-    document.getElementById(like-counter-1).innerHTML = (indexLike)
-    
+
+/*let startLike = posts.likes
+
+const bottoneLike = document.getElementsByClassName("like-button__label");
+for (let i = 0; i < bottoneLike.length; i++) {
+   const element = bottoneLike[i];
+   element.addEventListener("click", function() {
+       element.classList.toggle("like-button--liked");   
+   });
 }
+for (let i = 0; i < bottoneLike.length; i--) {
+    const element = bottoneLike[i];
+    element.addEventListener("click", function() {
+        element.classList.toggle("like-button--liked");
+        for (let i = startLike; i < 1000; i++) {
+            const indexLike =i;
+            console.log(indexLike)
+            i++
+            document.getElementById(like-counter-1).innerHTML = (indexLike) ;  
+        }
+    });
+ }*/
 
 
 //let eventoLike = document.querySelectorAll("<a>")
